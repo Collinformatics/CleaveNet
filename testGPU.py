bbf44b6fcf6e8a04916e54d4e2efd91f7bd45ecd
Collin Andrews <pizzaTrucks@outlook.com>
2026-02-18 09:49:38 -0600

    Add files via upload

---
+++ testGPU.py
@@ -0,0 +1,11 @@
+import torch
+
+# Select device
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
+print(f"Training Device: {device}\n")
