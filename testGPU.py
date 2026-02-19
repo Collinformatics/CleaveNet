fa6a988b0e746d0cd72f6c682f6e5ca424a2971a
Collin Andrews <pizzaTrucks@outlook.com>
2026-02-18 18:47:05 -0600

    Add files via upload

--- testGPU.py
+++ testGPU.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 
+print(f'TensorFlow: {tf}')
+print(f'TensorFlow File: {tf.__file__}')
+
 gpus = tf.config.list_physical_devices('GPU')
+print(f'TensorFlow Version: {tf.__version__}')
+print(f'GPU Found: {gpus}')
+
+from tensorflow.python.client import device_lib
+print(f'\nDevices:\n{device_lib.list_local_devices()}\n')
 
 if gpus:
     print("GPU(s) detected:")
@@ -10,3 +18,9 @@ if gpus:
 else:
     device = "CPU"
 print(f'Training Device: {device}\n')
+
+import os
+
+for k, v in os.environ.items():
+    if "CUDA" in k:
+        print(f"{k} = {v}")
